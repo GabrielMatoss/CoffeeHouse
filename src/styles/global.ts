d729b3234d3c0ba24f3267029caa3382fb1aa31f
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-02-19 21:16:58 -0300

    style(styles): added background color

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -8,7 +8,7 @@ export const GlobalStyle = createGlobalStyle`
     }
 
     body {
-        background-color: ${props => props.theme["purple-dark"]};
+        background-color: ${props => props.theme.background};
         color: ${props => props.theme.purple};
     }
 `;
