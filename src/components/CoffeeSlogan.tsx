6683e7e013045ac8e41c538b12306d246d6386dc
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-02-23 18:25:45 -0300

    feat(components): code refactoring

--- src/components/CoffeeSlogan.tsx
+++ src/components/CoffeeSlogan.tsx
@@ -1,48 +1,51 @@
-
 import imageCoffee from "../assets/coffee-delivery.png";
 import { Coffee, Package, ShoppingCart, Timer } from "phosphor-react";
 import { CoffeeSloganContent } from "./CoffeeSloganStyles";
 
 export function CoffeeSlogan() {
-    return (
-        <CoffeeSloganContent>
-            <div className="slogan">
-                <h1>Encontre o café perfeito para qualquer hora do dia</h1>
-                <p>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</p>
+  return (
+    <CoffeeSloganContent>
+      <div className="slogan">
+        <div className="title">
+          <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+          <p>
+            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+            hora
+          </p>
+        </div>
 
-                <div className="contentBeneficios">
-                        <span>
-                               <div className="contentIcons">
-                                     <ShoppingCart weight="fill" size={16}/>
-                                </div>
-                                <p>Compra simples e segura</p>
-                        </span>
-                             
-             
-                            <span>
-                                <div className="contentIcons">
-                                <Package weight="fill" size={16}/>
-                                </div>
-                                <p>Embalagem mantém o café intacto</p>
-                            </span>
-                            <span>
-                                <div className="contentIcons">
-                                <Timer size={16} weight="fill" />
-                                </div>
-                                <p>Entrega rápida e rastreada</p>
-                            </span>
-                            <span>
-                                <div className="contentIcons">
-                                <Coffee size={18} weight="fill" />
-                                </div>
-                                <p>O café chega fresquinho até você</p>
-                            </span>
-                </div>
-            </div>
+        <div className="contentItems">
+          <div>
+            <span className="contentIcons">
+              <ShoppingCart weight="fill" size={16} />
+            </span>
+            <p>Compra simples e segura</p>
+          </div>
 
-            <div className="imageCoffee">
-                <img className="imageCoffee" src={imageCoffee} alt="" />
-            </div>
-        </CoffeeSloganContent>
-    );
+          <div>
+            <span className="contentIcons">
+              <Package weight="fill" size={16} />
+            </span>
+            <p>Embalagem mantém o café intacto</p>
+          </div>
+          <div>
+            <span className="contentIcons">
+              <Timer size={16} weight="fill" />
+            </span>
+            <p>Entrega rápida e rastreada</p>
+          </div>
+          <div>
+            <span className="contentIcons">
+              <Coffee size={18} weight="fill" />
+            </span>
+            <p>O café chega fresquinho até você</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="imageCoffeeContent">
+        <img src={imageCoffee} alt="" />
+      </div>
+    </CoffeeSloganContent>
+  );
 }
--- src/components/CoffeeSloganStyles.ts
+++ src/components/CoffeeSloganStyles.ts
@@ -1,5 +1,80 @@
 import styled from "styled-components";
 
 export const CoffeeSloganContent = styled.div`
+  //background-color: brown;
 
+  margin-top: 4rem;
+  display: flex;
+  align-items: center;
+  gap: 3.5rem;
+
+  .slogan {
+    display: flex;
+    flex-direction: column;
+    //  background-color: blueviolet;
+    gap: 1.75rem;
+  }
+
+  .title {
+    // background-color: cadetblue;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+
+    h1 {
+      font-size: 3rem;
+      font-family: "Baloo 2", cursive;
+      line-height: 130%;
+    }
+
+    p {
+      font-size: 1.25rem;
+      font-weight: 400;
+      line-height: 130%;
+    }
+  }
+
+  .contentItems {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 0.75rem;
+
+
+    div:nth-child(1) span{
+        background: ${props => props.theme.purple};
+    } 
+
+
+    > div {
+      // background-color: aqua;
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+      gap: 0.75rem;
+
+      .contentIcons {
+        padding: 0.5rem;
+        border-radius: 9999px;
+         background-color: red;
+        display: inline-flex;
+      }
+    }
+
+    p {
+      font-size: 1rem;
+      font-weight: 400;
+      line-height: 130%;
+    }
+  }
+
+  .imageCoffeeContent {
+    display: flex;
+    align-items: flex-end;
+    justify-content: center;
+    // background-color: darkgreen;
+
+    > img {
+    width: 26.5rem;
+    }
+  }
 `;
