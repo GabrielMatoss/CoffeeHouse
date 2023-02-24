e38fc4065f9c55a3e83b33b1bb8015fb4da6eaa3
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-02-24 09:19:01 -0300

    style(styles): add a default text color

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,6 +9,7 @@ export const GlobalStyle = createGlobalStyle`
 
     body {
         background-color: ${(props) => props.theme.background};
+        color: ${props => props.theme["base-text"]};
         -webkit-font-smoothing: antialiased;
     }
 
