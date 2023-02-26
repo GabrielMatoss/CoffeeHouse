47171c89cb817628662203085987e6585f067249
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-02-26 11:11:26 -0300

    feat(coffeelist.tsx): added new cards for css creation

--- src/components/CoffeeList.tsx
+++ src/components/CoffeeList.tsx
@@ -1,42 +1,203 @@
-import { ShoppingCart } from "phosphor-react";
+import { Minus, Plus, ShoppingCart } from "phosphor-react";
 import { CoffeListStyles } from "./CoffeeListStyles";
 import imgCoffeeExpresso from "../assets/Expresso.png";
 
-export function CoffeeList () {
-    return (
-        <CoffeListStyles>
-            <h2>Nossos cafés</h2>
+export function CoffeeList() {
+  return (
+    <CoffeListStyles>
+      <div>
+        <h2>Nossos cafés</h2>
+      </div>
 
-            <div className="Card">
-                <div className="SectionCoffeImg">
-                    <img src={imgCoffeeExpresso} alt="" />
-                     <span>tradicional</span>
-                </div>
-               
+      <div className="CardContent">
+        <div className="Card">
+          <div className="SectionCoffeImg">
+            <img src={imgCoffeeExpresso} alt="" />
+            <span>tradicional</span>
+          </div>
 
-            <section className="SectionDescrition">
-                <h3>Expresso Tradicional</h3>
-                <p>O tradicional café feito com água quente e grãos moídos</p>
-            </section>
-                
+          <section className="SectionDescrition">
+            <h3>Expresso Tradicional</h3>
+            <p>O tradicional café feito com água quente e grãos moídos</p>
+          </section>
 
-                <section className="SectionPrice">
-                    <p>R$ 9,90</p>
-                    <div className="ContainerButtons">
-                    <div className="QtdButtons">
-                        <button /*onClick={}*/> - </button>
-                        <span>1</span>
-                        <button /*onClick={}*/> + </button>
-                    </div>
-                        
-                       
+          <section className="SectionPrice">
+            <p>9,90</p>
+            <div className="ContainerButtons">
+              <div className="QtdButtons">
+                <button /*onClick={}*/>
+                  <Minus size={16} weight="fill" />
+                </button>
+                <p>1</p>
+                <button /*onClick={}*/>
+                  <Plus size={20} weight="fill" />
+                </button>
+              </div>
 
-                        <div className="cart">
-                        <ShoppingCart weight="fill" size={16} />
-                        </div>
-                    </div>
-                </section>
+              <div className="cart">
+                <ShoppingCart weight="fill" size={20} />
+              </div>
             </div>
-        </CoffeListStyles>
-    );
+          </section>
+        </div>
+
+
+        <div className="Card">
+          <div className="SectionCoffeImg">
+            <img src={imgCoffeeExpresso} alt="" />
+            <span>tradicional</span>
+          </div>
+
+          <section className="SectionDescrition">
+            <h3>Expresso Tradicional</h3>
+            <p>O tradicional café feito com água quente e grãos moídos</p>
+          </section>
+
+          <section className="SectionPrice">
+            <p>9,90</p>
+            <div className="ContainerButtons">
+              <div className="QtdButtons">
+                <button /*onClick={}*/>
+                  <Minus size={16} weight="fill" />
+                </button>
+                <p>1</p>
+                <button /*onClick={}*/>
+                  <Plus size={20} weight="fill" />
+                </button>
+              </div>
+
+              <div className="cart">
+                <ShoppingCart weight="fill" size={20} />
+              </div>
+            </div>
+          </section>
+        </div>
+
+        <div className="Card">
+          <div className="SectionCoffeImg">
+            <img src={imgCoffeeExpresso} alt="" />
+            <span>tradicional</span>
+          </div>
+
+          <section className="SectionDescrition">
+            <h3>Expresso Tradicional</h3>
+            <p>O tradicional café feito com água quente e grãos moídos</p>
+          </section>
+
+          <section className="SectionPrice">
+            <p>9,90</p>
+            <div className="ContainerButtons">
+              <div className="QtdButtons">
+                <button /*onClick={}*/>
+                  <Minus size={16} weight="fill" />
+                </button>
+                <p>1</p>
+                <button /*onClick={}*/>
+                  <Plus size={20} weight="fill" />
+                </button>
+              </div>
+
+              <div className="cart">
+                <ShoppingCart weight="fill" size={20} />
+              </div>
+            </div>
+          </section>
+        </div>
+
+        <div className="Card">
+          <div className="SectionCoffeImg">
+            <img src={imgCoffeeExpresso} alt="" />
+            <span>tradicional</span>
+          </div>
+
+          <section className="SectionDescrition">
+            <h3>Expresso Tradicional</h3>
+            <p>O tradicional café feito com água quente e grãos moídos</p>
+          </section>
+
+          <section className="SectionPrice">
+            <p>9,90</p>
+            <div className="ContainerButtons">
+              <div className="QtdButtons">
+                <button /*onClick={}*/>
+                  <Minus size={16} weight="fill" />
+                </button>
+                <p>1</p>
+                <button /*onClick={}*/>
+                  <Plus size={20} weight="fill" />
+                </button>
+              </div>
+
+              <div className="cart">
+                <ShoppingCart weight="fill" size={20} />
+              </div>
+            </div>
+          </section>
+        </div>
+
+        <div className="Card">
+          <div className="SectionCoffeImg">
+            <img src={imgCoffeeExpresso} alt="" />
+            <span>tradicional</span>
+          </div>
+
+          <section className="SectionDescrition">
+            <h3>Expresso Tradicional</h3>
+            <p>O tradicional café feito com água quente e grãos moídos</p>
+          </section>
+
+          <section className="SectionPrice">
+            <p>9,90</p>
+            <div className="ContainerButtons">
+              <div className="QtdButtons">
+                <button /*onClick={}*/>
+                  <Minus size={16} weight="fill" />
+                </button>
+                <p>1</p>
+                <button /*onClick={}*/>
+                  <Plus size={20} weight="fill" />
+                </button>
+              </div>
+
+              <div className="cart">
+                <ShoppingCart weight="fill" size={20} />
+              </div>
+            </div>
+          </section>
+        </div>
+
+        <div className="Card">
+          <div className="SectionCoffeImg">
+            <img src={imgCoffeeExpresso} alt="" />
+            <span>tradicional</span>
+          </div>
+
+          <section className="SectionDescrition">
+            <h3>Expresso Tradicional</h3>
+            <p>O tradicional café feito com água quente e grãos moídos</p>
+          </section>
+
+          <section className="SectionPrice">
+            <p>9,90</p>
+            <div className="ContainerButtons">
+              <div className="QtdButtons">
+                <button /*onClick={}*/>
+                  <Minus size={16} weight="fill" />
+                </button>
+                <p>1</p>
+                <button /*onClick={}*/>
+                  <Plus size={20} weight="fill" />
+                </button>
+              </div>
+
+              <div className="cart">
+                <ShoppingCart weight="fill" size={20} />
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+      
+    </CoffeListStyles>
+  );
 }
