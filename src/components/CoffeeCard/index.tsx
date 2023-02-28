b3c793450adb537f442f9bd10d1aab9af61c112e
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-02-28 10:24:29 -0300

    feat(coffeecard): add new component and your styles

---
+++ src/components/CoffeeCard/index.tsx
@@ -0,0 +1,81 @@
+import { Minus, Plus, ShoppingCart } from "phosphor-react";
+import { CoffeeCards } from "./styles";
+import imgCoffeeExpresso from "../../assets/Expresso.png";
+import { useState } from "react";
+
+interface listCoffeeProps {
+  id: number;
+  name: string;
+  tags: string | string[];
+  description: string;
+  value: string;
+}
+export const listCoffee: listCoffeeProps[] = [
+  {
+    id: 1,
+    name: "Expresso Tradicional",
+    tags: "Tradicional",
+    description: "O tradicional café feito com água quente e grãos moídos",
+    value: "9,90",
+  },
+  {
+    id: 2,
+    name: "Expresso Americano",
+    tags: "Tradicional",
+    description: "Expresso diluído, menos intenso que o tradicional",
+    value: "9,90",
+  },
+  {
+    id: 3,
+    name: "Expresso Cremoso",
+    tags: "Tradicional",
+    description: "Café expresso tradicional com espuma cremosa",
+    value: "9,90",
+  },
+  {
+    id: 4,
+    name: "Expresso Gelado",
+    tags: ["Tradicional", "gelado"],
+    description: "Bebida preparada com café expresso e cubos de gelo",
+    value: "9,90",
+  },
+];
+
+export function CoffeeCard() {
+  const [cards, setCards] = useState<listCoffeeProps[]>(listCoffee);
+
+  return cards.map((values) => {
+    return (
+      <CoffeeCards key={values.id}>
+        <img src={imgCoffeeExpresso} alt="" />
+        <div className="SectionCoffeeTag">
+          <span>{values.tags}</span>
+        </div>
+
+        <section className="SectionDescrition">
+          <h3>{values.name}</h3>
+          <p>{values.description}</p>
+        </section>
+
+        <section className="SectionPrice">
+          <p>{values.value}</p>
+          <div className="ContainerButtons">
+            <div className="QtdButtons">
+              <button /*onClick={}*/>
+                <Minus size={16} weight="fill" />
+              </button>
+              <p>1</p>
+              <button /*onClick={}*/>
+                <Plus size={20} weight="fill" />
+              </button>
+            </div>
+
+            <div className="cart">
+              <ShoppingCart weight="fill" size={20} />
+            </div>
+          </div>
+        </section>
+      </CoffeeCards>
+    );
+  });
+}
---
+++ src/components/CoffeeCard/styles.ts
@@ -0,0 +1,118 @@
+import styled from "styled-components";
+
+export const CoffeeCards = styled.div`
+    position: relative;
+    margin-top: 3rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    text-align: center;
+    gap: 0.75rem;
+    padding-inline: 15px;
+    background: ${(props) => props.theme["base-card"]};
+    width: 16rem;
+    height: 19.37rem;
+    border-radius: 0 20px 0 20px;
+
+    > img {
+      margin-top: -1.3rem;
+    }
+
+    .SectionCoffeeTag span {
+      padding: 4px 8px;
+      border-radius: 100px;
+
+      background: ${(props) => props.theme["yellow-light"]};
+      color: ${(props) => props.theme["yellow-dark"]};
+
+      font-size: 0.625rem;
+      font-weight: 700;
+      text-transform: uppercase;
+    }
+
+    .SectionDescrition {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      gap: 8px;
+    }
+
+    .SectionDescrition h3 {
+      font-weight: 700;
+      font-family: "Baloo 2", cursive;
+      font-size: 1.5rem;
+      line-height: 130%;
+      color: ${(props) => props.theme["base-subtitle"]};
+    }
+
+    .SectionDescrition p {
+      text-align: center;
+      font-size: .95rem;
+      color: ${(props) => props.theme["base-label"]};
+    }
+
+    .SectionPrice {
+      display: flex;
+      width: 100%;
+      align-items: center;
+      justify-content: space-between;
+      margin-top: 1rem;
+      margin-bottom: 1rem;
+
+      > p {
+        font-weight: 800;
+        font-family: "Baloo 2", cursive;
+        font-size: 1.5rem;
+      }
+
+      > p::before {
+        content: "R$";
+        font-size: 0.9rem;
+        font-weight: 400;
+        margin-right: 0.2rem;
+        font-family: "Roboto", sans-serif;
+      }
+
+      .ContainerButtons {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        gap: 1rem;
+
+        button {
+          border: none;
+          background: none;
+          color: ${(props) => props.theme.purple};
+          font-size: 1rem;
+        }
+
+        .QtdButtons {
+          text-align: center;
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          gap: 5px;
+          background-color: ${(props) => props.theme["base-button"]};
+          padding: 8px 10px;
+          border-radius: 6px;
+          text-align: center;
+          p {
+            font-weight: 400;
+          }
+        }
+
+        .cart {
+          background: ${(props) => props.theme["purple-dark"]};
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          border-radius: 8px;
+          color: ${(props) => props.theme.background};
+          padding: 10px;
+        }
+      }
+    }
+`;
+
