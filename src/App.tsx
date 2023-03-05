1a4cf84dc847db59d3626252e7341f8fe1ca1a62
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-03-05 20:30:03 -0300

    feat(src): installation of react-router-dom packages and their configuration in the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { Content } from "./components/ContentPage";
+import { Router } from "./Router";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+      <Router />
+      </BrowserRouter>
     <Content />
     <GlobalStyle />
     </ThemeProvider>
