4148ad855f660cb6079c5ca738e2b58948f71530
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-03-10 18:40:16 -0300

    refactor(carthistory): refactoring html css code

--- src/pages/CartHistory/index.tsx
+++ src/pages/CartHistory/index.tsx
@@ -1,46 +1,76 @@
-import { MapPinLine } from "phosphor-react";
+import { MapPinLine, Minus, Plus, Trash, TrashSimple } from "phosphor-react";
 import { ContainerCartHistory } from "./styles";
 
-export function CartHistory () {
-    return(
+import caféComLeite from "../../assets/cafeComLeite.png";
+import chocolateQuente from "../../assets/chocolateQuente.png";
+
+
+export function CartHistory() {
+  return (
     <ContainerCartHistory>
-        <div className="contentInfos">
-            <h3>Complete seu pedido</h3>
-            <form action="">
-                <div className="headerForm">
-                <MapPinLine size={24}/>
-                <div>
-                    <p>Endereço de entrega</p>
-                    <p>Informe onde deseja receber seu pedido</p>
-                </div>
-                </div>
-
-                <input type="text" name="" id="" />
-                <input type="text" name="" id="" />
-
-                <div>
-                <input type="text" name="" id="" />
-                <input type="text" name="" id="" />
-                </div>
-
-                <div>
-                <input type="text" name="" id="" />
-                <input type="text" name="" id="" />
-                <input type="text" name="" id="" />
-                </div>
-            </form>
-
-            <div className="methodPayment">
-            <p>Metodos de pagamento</p>
+      <div className="contentInfos">
+        <h3>Complete seu pedido</h3>
+        <form action="">
+          <div className="headerForm">
+            <MapPinLine size={24} />
+            <div>
+              <p>Endereço de entrega</p>
+              <p>Informe onde deseja receber seu pedido</p>
             </div>
-        </div>
+          </div>
 
-        <div className="contentValues">
-            <h3>Cafés relacionados</h3>
-            <div className="valuesTotal">
-                <p>é um teste</p>
-            </div>
+          <input type="text" name="" id="" />
+          <input type="text" name="" id="" />
+
+          <div>
+            <input type="text" name="" id="" />
+            <input type="text" name="" id="" />
+          </div>
+
+          <div>
+            <input type="text" name="" id="" />
+            <input type="text" name="" id="" />
+            <input type="text" name="" id="" />
+          </div>
+        </form>
+
+        <div className="methodPayment">
+          <p>Metodos de pagamento</p>
         </div>
+      </div>
+
+      <div className="contentValues">
+        <h3>Cafés relacionados</h3>
+        <div className="valuesTotal">
+          <div className="queryCoffees">
+                <img src={caféComLeite} alt="" />
+                <div className="teste">
+                 
+                  <div>
+                  <p>Expresso Tradicional</p>
+                  <div className="containerButtons">
+                    <div className="buttonsQtd">
+                    <button /*onClick={}*/>
+                      <Minus size={16} weight="fill" />
+                      </button>
+                      <p>1</p>
+                     <button /*onClick={}*/>
+                      <Plus size={20} weight="fill" />
+                    </button>
+                    </div>
+                     
+                    <button>
+                      <Trash size={20} />
+                      Remover
+                     </button>
+                  </div>
+                   
+                  </div>
+                  <p>R$ 9,90</p>
+                </div>
+          </div>
+        </div>
+      </div>
     </ContainerCartHistory>
-    )
+  );
 }
--- src/pages/CartHistory/styles.ts
+++ src/pages/CartHistory/styles.ts
@@ -1,29 +1,57 @@
 import styled from "styled-components";
 
 export const ContainerCartHistory = styled.main`
-    width: 100%;
-    display: grid;
-    grid-template-columns: 2fr 1fr;
-    gap: 1rem;
-    background-color: darkcyan;
-    margin-top: 3.5rem;
+  width: 100%;
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  gap: 1rem;
+   background-color: darkcyan;
+  margin-top: 3.5rem;
 
-    h3 {
-        font-weight: 700;
-        font-family: "Baloo 2", cursive;
-        font-size: 1.2rem;
-    }
- 
+  h3 {
+    font-weight: 700;
+    font-family: "Baloo 2", cursive;
+    font-size: 1.2rem;
+  }
 
-   form {
+  form {
     background-color: cadetblue;
     padding: 40px;
-   }
+  }
 
-    .headerForm {
-        background-color: brown;
-        display: flex;
-        align-items: flex-start;
-        justify-content: flex-start;
+  .headerForm {
+    background-color: brown;
+    display: flex;
+    align-items: flex-start;
+    justify-content: flex-start;
+    svg {
+      color: ${(props) => props.theme["yellow-dark"]};
     }
+  }
+
+  .valuesTotal {
+    background-color: cadetblue;
+    display: flex;
+    flex-direction: column;
+
+    .buttonsQtd {
+      display: flex;
+    }
+    .teste {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+    .queryCoffees {
+      display: flex;
+      background-color: chocolate;
+
+    }
+    .containerButtons {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      
+    }
+  }
 `;
