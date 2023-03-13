3567d2727ca733f3ac083dcc4d03273ffa5f6bc7
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-03-13 09:44:10 -0300

    style(styles.ts): refactoring component css

--- src/pages/CartHistory/styles.ts
+++ src/pages/CartHistory/styles.ts
@@ -5,7 +5,7 @@ export const ContainerCartHistory = styled.main`
   display: grid;
   grid-template-columns: 2fr 1fr;
   gap: 1rem;
-   background-color: darkcyan;
+  background-color: darkcyan;
   margin-top: 3.5rem;
 
   h3 {
@@ -20,38 +20,72 @@ export const ContainerCartHistory = styled.main`
   }
 
   .headerForm {
-    background-color: brown;
+    // background-color: brown;
+    gap: 0.5rem;
     display: flex;
     align-items: flex-start;
     justify-content: flex-start;
+
+    h3 {
+      font-family: "Roboto", sans-serif;
+    }
     svg {
       color: ${(props) => props.theme["yellow-dark"]};
     }
   }
 
-  .valuesTotal {
-    background-color: cadetblue;
+  .formContainer {
     display: flex;
     flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    gap: 1rem;
 
-    .buttonsQtd {
-      display: flex;
+    #Rua {
+      width: 100%;
     }
-    .teste {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-    }
-    .queryCoffees {
-      display: flex;
-      background-color: chocolate;
+  }
 
-    }
-    .containerButtons {
+  .contentValues {
+    // background-color: burlywood;
+
+    .container {
       display: flex;
-      align-items: center;
-      justify-content: center;
-      
+      flex-direction: column;
+      gap: 1rem;
+      padding: 1rem;
+
+      .contentCardCoffee {
+        display: flex;
+        // background-color: blue;
+        align-items: center;
+        justify-content: space-evenly;
+        width: 100%;
+        border-bottom: 2px solid grey;
+        padding-bottom: 1rem;
+        img {
+          width: 5rem;
+          height: 5rem;
+        }
+
+        .contentPropsCoffee {
+          display: flex;
+          flex-direction: column;
+          .nameAndPrice {
+            display: flex;
+
+            width: 100%;
+            // background-color: burlywood;
+            gap: 3rem;
+          }
+
+          .containerButtons {
+            margin-top: 1rem;
+            display: flex;
+            gap: 1rem;
+          }
+        }
+      }
     }
   }
 `;
