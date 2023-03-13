073a26944bd2bce8ffbcb9d6b442c92dfbfbb8f7
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-03-13 09:43:00 -0300

    refactor(carthistory): refactoring Order Box Html

--- src/pages/CartHistory/index.tsx
+++ src/pages/CartHistory/index.tsx
@@ -4,23 +4,22 @@ import { ContainerCartHistory } from "./styles";
 import caféComLeite from "../../assets/cafeComLeite.png";
 import chocolateQuente from "../../assets/chocolateQuente.png";
 
-
 export function CartHistory() {
   return (
     <ContainerCartHistory>
       <div className="contentInfos">
         <h3>Complete seu pedido</h3>
-        <form action="">
+        <form action="" className="formContainer">
           <div className="headerForm">
-            <MapPinLine size={24} />
+            <MapPinLine size={25} />
             <div>
-              <p>Endereço de entrega</p>
-              <p>Informe onde deseja receber seu pedido</p>
+              <h4>Endereço de entrega</h4>
+              <p>Informe o endereço onde deseja receber seu pedido</p>
             </div>
           </div>
 
-          <input type="text" name="" id="" />
-          <input type="text" name="" id="" />
+          <input type="text" name="" id="CEP" />
+          <input type="text" name="" id="Rua" />
 
           <div>
             <input type="text" name="" id="" />
@@ -42,27 +41,27 @@ export function CartHistory() {
       <div className="contentValues">
         <h3>Cafés relacionados</h3>
         <div className="container">
-            <div className="contentCardCoffee">
-                <img src={caféComLeite} />
+          <div className="contentCardCoffee">
+            <img src={caféComLeite} />
+            <div className="contentPropsCoffee">
+              <div className="nameAndPrice">
+                <h4>Café com Leite</h4>
+                <p>R$ 5,00</p>
+              </div>
+
+              <div className="containerButtons">
                 <div>
-                  <div>
-                    <h6>Café com Leite</h6>
-                    <p>R$ 5,00</p>
-                  </div>
-
-                  <div className="containerButtons">
-                    <div>
-                      <button>+</button>
-                      <span>1</span>
-                      <button>-</button>
-                    </div>
-
-                    <button>
-                      <span>Remover</span>
-                    </button>
-                  </div>
+                  <button>+</button>
+                  <span>1</span>
+                  <button>-</button>
                 </div>
+
+                <button>
+                  <span>Remover</span>
+                </button>
+              </div>
             </div>
+          </div>
         </div>
       </div>
     </ContainerCartHistory>
