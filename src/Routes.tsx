75918d407b76f603b1f96eaceb5bac1d5edd0497
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-03-20 18:18:51 -0300

    feat(routes): adding default layout route as context

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,13 +1,16 @@
 import { Route } from "react-router";
 import { Routes } from "react-router-dom";
+import { DefaultLayout } from "./layouts/DefaultLayout";
 import { CompleteOrder } from "./pages/CompleteOrder";
 import { Home } from "./pages/Home";
 
-export function Router(){
-return(
-   <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/completeOrder" element={<CompleteOrder />} />
-   </Routes>
-)
+export function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/completeOrder" element={<CompleteOrder />} />
+      </Route>
+    </Routes>
+  );
 }
