96558d6e4fb23f604c0f47f09b939fa1046dfaf5
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-03-21 16:58:02 -0300

    feat(hero): add perk component, along with their typings and styles
    I used the Hook useTheme to help me with the background colors of the icons

--- src/pages/Home/components/Hero/index.tsx
+++ src/pages/Home/components/Hero/index.tsx
@@ -1,19 +1,48 @@
-import { HeroContainer, HeroContent } from "./styles";
+import { BenefitsContainer, HeroContainer, HeroContent } from "./styles";
 import heroCoffeeDelivery from "../../../../assets/heroCoffeeDelivery.png";
-
-export function Hero(){
-   return(
+import { InfoWithIcon } from "../../../../components/InfoWithIcon";
+import { Coffee, Package, ShoppingCart, Timer } from "phosphor-react";
+import { useTheme } from "styled-components";
+export function Hero() {
+  const { colors } = useTheme();
+  return (
     <HeroContainer>
-       <HeroContent className="container">
-            <div>
-                <section>
-                    <h1>Encontre o café perfeito para qualquer hora do dia</h1>
-                    <p>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</p>
-                </section>
-            </div>
+      <HeroContent className="container">
+        <div>
+          <section>
+            <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+            <p>
+              Com o Coffee Delivery você recebe seu café onde estiver, a
+              qualquer hora
+            </p>
+          </section>
 
-            <img src={heroCoffeeDelivery} />
-       </HeroContent>
+          <BenefitsContainer>
+            <InfoWithIcon
+              iconBg={colors["yellow-dark"]}
+              text="Compra simples e segura"
+              icon={<ShoppingCart size={18} weight="fill" />}
+            />
+            <InfoWithIcon
+              iconBg={colors["base-text"]}
+              text="Embalagem mantém o café intacto"
+              icon={<Package size={18} weight="fill" />}
+            />
+            <InfoWithIcon
+              iconBg={colors["yellow"]}
+              text="Entrega rápida e rastreada"
+              icon={<Timer size={18} weight="fill" />}
+            />
+            <InfoWithIcon
+              iconBg={colors["purple"]}
+              text="O café chega fresquinho até você"
+              icon={<Coffee size={18} weight="fill" />}
+            />
+          </BenefitsContainer>
+        </div>
+
+        <img src={heroCoffeeDelivery} />
+      </HeroContent>
     </HeroContainer>
-   )
+  );
 }
--- src/pages/Home/components/Hero/styles.ts
+++ src/pages/Home/components/Hero/styles.ts
@@ -8,11 +8,14 @@ export const HeroContainer = styled.section`
     background: ${({ theme }) => `url(${heroBackground}) no-repeat center,
       linear-gradient(
         0deg,
-        ${theme.white} 0%,
-        ${rgba(theme.background, 0.2)} 50%,
-        ${theme.background} 100%
+        ${theme.colors.white} 0%,
+        ${rgba(theme.colors.background, 0.2)} 50%,
+        ${theme.colors.background} 100%
       )`};
       background-size: cover;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `;
 
 export const HeroContent = styled.div`
@@ -24,12 +27,27 @@ export const HeroContent = styled.div`
 font-family: 'Roboto', sans-serif; */
     div section{
         h1{
-            color: ${({theme}) => theme["base-title"]};
+            color: ${({theme}) => theme.colors["base-title"]};
             font-size: 3rem;
             font-family: 'Baloo 2', cursive;
             line-height: 130%;
             font-weight: 800;
             margin-bottom: 1rem;
         }
+
+        p{
+            color: ${({theme}) => theme.colors["base-subtitle"]};
+            font-size: 1.25rem;
+            line-height: 130%;
+            font-weight: 400;
+        }
     }
+`;
+
+export const BenefitsContainer = styled.div`
+    width: 100%;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    row-gap: 1.25rem;
+    margin-top: 4.125rem;
 `;
