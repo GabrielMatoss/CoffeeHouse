f199ffe3e2fc7abd8fc2bf354b154b6a99c4bb6f
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-03-25 15:57:01 -0300

    feat(coffeecard): passing the information received by the component, in the html tags

--- src/pages/Home/components/CoffeeCard/index.tsx
+++ src/pages/Home/components/CoffeeCard/index.tsx
@@ -1,48 +1,47 @@
 import {
   AddCartWrapper,
-    CardFooter,
+  CardFooter,
   CoffeeCardContainer,
   DescriptionCoffee,
   NameCoffee,
   Tags,
 } from "./styles";
-import { listCoffee } from "../../../../utils/CoffeeData";
 import { QuantityInput } from "../../../../components/QuantityInput";
 import { ShoppingCart } from "phosphor-react";
-export function CoffeeCard() {
+import { ListCoffeeProps } from "../../../../utils/CoffeeData";
+import { formatMoney } from "../../../../utils/formatMoney";
+interface CoffeeCardProps {
+  coffee: ListCoffeeProps;
+}
+
+export function CoffeeCard({ coffee }: CoffeeCardProps) {
+  const formattedPrice = formatMoney(coffee.value);
+
   return (
-    <>
-      {listCoffee.map((e) => {
-        return (
-          <CoffeeCardContainer key={e.id}>
-            <img src={e.src} alt="" />
+    <CoffeeCardContainer>
+      <img src={`/coffees/${coffee.src}`} alt="" />
 
-            <Tags>
-           {
-            listCoffee.map((teste) => {
-             return <span key={`${teste.name}${teste.tags}`}>{teste.tags}</span>
-            })
-           }
-            </Tags>
+      <Tags>
+        {coffee.tags.map((tag) => (
+          <span key={`${coffee.id}${tag}`}>{tag}</span>
+        ))}
+      </Tags>
 
-            <NameCoffee>{e.name}</NameCoffee>
-            <DescriptionCoffee>{e.description}</DescriptionCoffee>
+      <NameCoffee>{coffee.name}</NameCoffee>
+      <DescriptionCoffee>{coffee.description}</DescriptionCoffee>
 
-            <CardFooter>
-              <div>
-                <span>R$</span>
-                <p>9,90</p>
-              </div>
-              <AddCartWrapper>
-                <QuantityInput />
-                <button>
-                  <ShoppingCart size={22} weight="fill"/>
-                </button>
-              </AddCartWrapper>
-            </CardFooter>
-          </CoffeeCardContainer>
-        );
-      })}
-    </>
+      <CardFooter>
+        <div>
+          <span>R$</span>
+          <p>{formattedPrice}</p>
+        </div>
+        <AddCartWrapper>
+          <QuantityInput />
+          <button>
+            <ShoppingCart size={22} weight="fill" />
+          </button>
+        </AddCartWrapper>
+      </CardFooter>
+    </CoffeeCardContainer>
   );
 }
