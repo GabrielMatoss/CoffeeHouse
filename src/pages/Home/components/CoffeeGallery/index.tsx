48f9d05360dfffa2f94df9580af2523c3600a286
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-03-25 15:56:00 -0300

    feat(coffeegallery): add CoffeeCard component
    add CoffeeCard component, applying a map and returning the component for each ListCoffee item

--- src/pages/Home/components/CoffeeGallery/index.tsx
+++ src/pages/Home/components/CoffeeGallery/index.tsx
@@ -1,15 +1,17 @@
+import { listCoffee } from "../../../../utils/CoffeeData";
 import { CoffeeCard } from "../CoffeeCard";
 import { CoffeeGalleryContainer, CoffeeList } from "./styles";
 
-export function CoffeeGallery(){
-    return(
-        <CoffeeGalleryContainer className="container">
-            <h3>Nossos Cafés</h3>
-            
-            <CoffeeList>
-                <CoffeeCard />
-            </CoffeeList>
-            
-        </CoffeeGalleryContainer>
-    );
+export function CoffeeGallery() {
+  return (
+    <CoffeeGalleryContainer className="container">
+      <h3>Nossos Cafés</h3>
+
+      <CoffeeList>
+        {listCoffee.map((coffee) => (
+          <CoffeeCard key={coffee.id} coffee={coffee} />
+        ))}
+      </CoffeeList>
+    </CoffeeGalleryContainer>
+  );
 }
