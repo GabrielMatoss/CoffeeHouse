34b0b1a8f9e60d751bfc7869532f7e0307f97f60
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-03-29 18:11:57 -0300

    feat(completeorderform): add components FormContainer,SectionTitle and AddressForm

--- src/pages/CompleteOrder/components/CompleteOrderForm/index.tsx
+++ src/pages/CompleteOrder/components/CompleteOrderForm/index.tsx
@@ -1,9 +1,25 @@
-import { CompleteOrderFormContainer } from "./styles";
+import { MapPinLine } from "phosphor-react";
+import { useTheme } from "styled-components";
+import { SectionTitle } from "../SectionTitle";
+import { AddressForm } from "./AddressForm";
+import { CompleteOrderFormContainer, FormSectionContainer } from "./styles";
 
 export function CompleteOrderForm(){
+    const { colors } = useTheme();
+
     return(
         <CompleteOrderFormContainer>
             <h3>Complete seu pedido</h3>
+
+            <FormSectionContainer>
+                <SectionTitle 
+                title="Endereço de Entrega"
+                subtitle="Informe o endereço onde deseja receber seu pedido"
+                icon={<MapPinLine color={colors["yellow-dark"]} size={22}/>}
+                />
+
+                <AddressForm />
+            </FormSectionContainer>
         </CompleteOrderFormContainer>
     );
 }
