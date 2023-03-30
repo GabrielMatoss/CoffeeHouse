12ee46ace9d4ed0bc0212b7d9a71705960f8c025
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-03-30 11:55:48 -0300

    feat(addressform.tsx): add Input component on AddressForm passing its properties

--- src/pages/CompleteOrder/components/CompleteOrderForm/AddressForm.tsx
+++ src/pages/CompleteOrder/components/CompleteOrderForm/AddressForm.tsx
@@ -1,9 +1,16 @@
+import { Input } from "../../../../components/Input";
 import { AddressFormContainer } from "./styles";
 
 export function AddressForm(){
     return(
         <AddressFormContainer>
-            <input type="text" />
+           <Input placeholder="CEP" className="cep" type="number"/>
+           <Input placeholder="Rua" className="rua" />
+           <Input placeholder="Número" type="number" />
+           <Input placeholder="Completemento" className="complemento" />
+           <Input placeholder="Bairro" />
+           <Input placeholder="Cidade" />
+           <Input placeholder="UF" />
         </AddressFormContainer>
     );
 }
