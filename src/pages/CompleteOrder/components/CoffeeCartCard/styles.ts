09fb0d0df98658d15c96d78a4d8a43d107b4da49
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-04-06 15:16:14 -0300

    feat(coffeecartcard): add remove order button, and its styles

--- src/pages/CompleteOrder/components/CoffeeCartCard/styles.ts
+++ src/pages/CompleteOrder/components/CoffeeCartCard/styles.ts
@@ -1,14 +1,14 @@
 import styled from "styled-components";
 
 export const CoffeeCartCardContainer = styled.div`
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
-    border-bottom: 1px solid ${({theme}) => theme.colors["base-button"]};
-    padding-bottom: 1.5rem;
-    margin-bottom: 1.5rem;
+  border-bottom: 1px solid ${({ theme }) => theme.colors["base-button"]};
+  padding-bottom: 1.5rem;
+  margin-bottom: 1.5rem;
 
   p {
     font-family: "Roboto", sans-serif;
@@ -23,8 +23,8 @@ export const CoffeeCartCardContainer = styled.div`
     align-items: center;
     gap: 1.25rem;
     img {
-        width: 4rem;
-        height: 4rem;
+      width: 4rem;
+      height: 4rem;
     }
   }
 
@@ -35,16 +35,36 @@ export const CoffeeCartCardContainer = styled.div`
 `;
 
 export const ActionsContainer = styled.div`
-    margin-top: 0.5rem;
-    height: 2rem;
-    display: flex;
-    align-items: center;
-    gap: 0.5rem;
+  margin-top: 0.5rem;
+  height: 2rem;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
 
-    > div {
-        max-width: 4.5rem;
-        height: 100%;
-    }
+  > div {
+    max-width: 4.5rem;
+    height: 100%;
+  }
 `;
- 
-export const RemoveButton = styled.button``;
+
+export const RemoveButton = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 0.25rem;
+  color: ${({ theme }) => theme.colors["base-text"]};
+  font-size: 0.75rem;
+  height: 100%;
+  border: none;
+  background: ${({ theme }) => theme.colors["base-button"]};
+  padding: 0 0.5rem;
+  border-radius: 6px;
+  transition: 0.4s;
+
+  svg {
+    color: ${({ theme }) => theme.colors.purple};
+  }
+
+  &:hover {
+    background: ${({ theme }) => theme.colors["base-hover"]};
+  }
+`;
