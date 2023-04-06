88b7533981c8adca57fd5ec3fa477afef621919e
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-04-06 18:14:23 -0300

    feat(routes.tsx): add the routes the OrderConfirmed component

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,6 +3,7 @@ import { Routes } from "react-router-dom";
 import { DefaultLayout } from "./layouts/DefaultLayout";
 import { CompleteOrder } from "./pages/CompleteOrder";
 import { Home } from "./pages/Home";
+import { OrderConfirmed } from "./pages/OrderConfirmed";
 
 export function Router() {
   return (
@@ -10,6 +11,7 @@ export function Router() {
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/completeOrder" element={<CompleteOrder />} />
+        <Route path="/orderConfirmed" element={<OrderConfirmed />}/>
       </Route>
     </Routes>
   );
