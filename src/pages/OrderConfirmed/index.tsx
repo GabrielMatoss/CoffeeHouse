87a14327404d91cfccc4bbde5b3ac0828788f78a
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-04-08 11:02:30 -0300

    feat(orderconfirmed): add component InfoWithIcon

--- src/pages/OrderConfirmed/index.tsx
+++ src/pages/OrderConfirmed/index.tsx
@@ -1,20 +1,57 @@
 import { OrderConfirmedContainer, OrderDetailsContainer } from "./styles";
 import confirmedOrderIllustrarion from "../../assets/confirmedOrder.svg";
+import { InfoWithIcon } from "../../components/InfoWithIcon";
+import { Clock, CurrencyDollar, MapPin, Money } from "phosphor-react";
+import { useTheme } from "styled-components";
 
-export function OrderConfirmed(){
-    return (
-        <OrderConfirmedContainer className="container">
-            <div>
-                <h1>Uhu! Pedido confirmado</h1>
-                <p>Agora é só aguardar que logo o café chegará até você</p>
-            </div>
+export function OrderConfirmed() {
+  const { colors } = useTheme();
+  return (
+    <OrderConfirmedContainer className="container">
+      <div>
+        <h1>Uhu! Pedido confirmado</h1>
+        <p>Agora é só aguardar que logo o café chegará até você</p>
+      </div>
 
-            <section>
-            <OrderDetailsContainer>
-                <p>Olá</p>
-            </OrderDetailsContainer>
-                <img src={confirmedOrderIllustrarion}/>
-            </section>
-        </OrderConfirmedContainer>
-    );
+      <section>
+        <OrderDetailsContainer>
+          <InfoWithIcon
+            icon={<MapPin weight="fill" />}
+            iconBg={colors.purple}
+            text={
+              <p>
+                Entrega em <strong>Rua João Daniel Martinelli, 102</strong>
+                <br /> Farrapos - Porto Alegre, RS
+              </p>
+            }
+          />
+
+          <InfoWithIcon
+            icon={<Clock weight="fill" />}
+            iconBg={colors.yellow}
+            text={
+              <p>
+                Previsão de Entrega
+                <br />
+                <strong>20 min - 30 min</strong>
+              </p>
+            }
+          />
+
+          <InfoWithIcon
+            icon={<CurrencyDollar weight="fill" />}
+            iconBg={colors["yellow-dark"]}
+            text={
+              <p>
+                Pagamento na entrega
+                <br />
+                <strong>Cartão de Crédito</strong>
+              </p>
+            }
+          />
+        </OrderDetailsContainer>
+        <img src={confirmedOrderIllustrarion} />
+      </section>
+    </OrderConfirmedContainer>
+  );
 }
