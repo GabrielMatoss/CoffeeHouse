be8cb1f29fea7d2427fa185d5c411ccf86724dd1
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-04-11 14:25:17 -0300

    feat(selectedcoffees): using the useCart hook and destructuring the cartItem
    using the useCart hook and destructuring the cartItems, making a map and displaying a CoffeeCartCard
    for each item in it

--- src/pages/CompleteOrder/components/SelectedCoffees/index.tsx
+++ src/pages/CompleteOrder/components/SelectedCoffees/index.tsx
@@ -1,16 +1,18 @@
+import { useCart } from "../../../../hooks/useCart";
 import { CoffeeCartCard } from "../CoffeeCartCard";
 import { ConfirmationSection } from "./ConfirmationSection";
 import { DetailsContainer, SelectedCoffeesContainer } from "./styles";
 
 export function SelectedCoffees() {
+  const { cartItems } = useCart();
   return (
     <SelectedCoffeesContainer>
       <h3>Cafés selecionados</h3>
 
       <DetailsContainer>
-        <CoffeeCartCard />
-        <CoffeeCartCard />
-
+        {cartItems.map((item) => (
+          <CoffeeCartCard key={item.id} coffee={item} />
+        ))}
         <ConfirmationSection />
       </DetailsContainer>
     </SelectedCoffeesContainer>
