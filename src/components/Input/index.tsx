71e5451943d5cefc9683ffe09b7a5fbbd2cbd2eb
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-04-11 14:28:19 -0300

    feat(input): add error checking code, and error message css style

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,10 +1,17 @@
-import { InputHTMLAttributes } from "react";
-import { InputStyleContainer } from "./styles";
+import { InputHTMLAttributes, forwardRef } from "react";
+import { InputStyleContainer, InputWrapper } from "./styles";
 
-type InputProps = InputHTMLAttributes<HTMLInputElement>;
+type InputProps = InputHTMLAttributes<HTMLInputElement> & {
+  error?: string;
+};
 
-export function Input({...props}: InputProps){
-    return(
-        <InputStyleContainer {...props}/>
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ error, className, ...props }, ref) => {
+    return (
+      <InputWrapper className={className}>
+        <InputStyleContainer {...props} ref={ref} />
+        {error && <p>{error}</p>}
+      </InputWrapper>
     );
-}
+  }
+);
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,24 +1,36 @@
 import styled from "styled-components";
 
+export const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.3rem;
+  position: relative;
+
+  > p {
+    color: #f84747;
+    font-size: 0.875rem;
+  }
+`;
+
 export const InputStyleContainer = styled.input`
-    height: 2.625rem;
-    border-radius: 4px;
-    border: 1px solid ${({theme}) => theme.colors["base-button"]};
-    background: ${({theme}) => theme.colors["base-input"]};
-    color: ${({theme}) => theme.colors["base-text"]};
+  height: 2.625rem;
+  border-radius: 4px;
+  border: 1px solid ${({ theme }) => theme.colors["base-button"]};
+  background: ${({ theme }) => theme.colors["base-input"]};
+  color: ${({ theme }) => theme.colors["base-text"]};
 
-    outline: none;
+  outline: none;
 
-    font-size: 0.75rem;
-    padding: 0 0.75rem;
+  font-size: 0.75rem;
+  padding: 0 0.75rem;
 
-    transition: 0.4s;
+  transition: 0.4s;
 
-    &:focus {
-        border-color: ${({theme}) => theme.colors["yellow-dark"]};
-    }
+  &:focus {
+    border-color: ${({ theme }) => theme.colors["yellow-dark"]};
+  }
 
-    &::placeholder{
-        color: ${({theme}) => theme.colors["base-label"]};
-    }
+  &::placeholder {
+    color: ${({ theme }) => theme.colors["base-label"]};
+  }
 `;
