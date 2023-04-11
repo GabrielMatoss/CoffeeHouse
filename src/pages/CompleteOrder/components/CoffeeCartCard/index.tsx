7b910906b77965fea774fb9e43638cab237a40bc
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-04-11 14:20:25 -0300

    feat(coffeecartcard): add functions for subtraction, addition and removal of selected coffees
    add subtraction, addition and removal functions for selected coffees, passing them on to the
    QuantityInput component

--- src/pages/CompleteOrder/components/CoffeeCartCard/index.tsx
+++ src/pages/CompleteOrder/components/CoffeeCartCard/index.tsx
@@ -5,24 +5,52 @@ import {
 } from "./styles";
 import { QuantityInput } from "../../../../components/QuantityInput";
 import { Trash } from "phosphor-react";
-import capuccino from "/coffees/capuccino.png";
-export function CoffeeCartCard() {
+import { CartItem } from "../../../../contexts/CartContext";
+import { formatMoney } from "../../../../utils/formatMoney";
+import { useCart } from "../../../../hooks/useCart";
+
+interface CoffeeCartCardProps {
+  coffee: CartItem;
+}
+
+export function CoffeeCartCard({ coffee }: CoffeeCartCardProps) {
+  const { changeCartItemQuantity, removeCartItem } = useCart();
+  const coffeeTotal = coffee.value * coffee.quantity;
+  const formattedPrice = formatMoney(coffeeTotal);
+
+  function handleIncrease() {
+    changeCartItemQuantity(coffee.id, "increase");
+  }
+
+  function handleDecrease() {
+    changeCartItemQuantity(coffee.id, "decrease");
+  }
+
+  function handleRemove(){
+   removeCartItem(coffee.id)
+  }
+
   return (
     <CoffeeCartCardContainer>
       <div>
-        <img src={capuccino} alt="" />
+        <img src={`/coffees/${coffee.src}`} alt="" />
         <div>
-          <p>Expresso Tradicional</p>
+          <p>{coffee.name}</p>
           <ActionsContainer>
-            <QuantityInput size="small" />
-            <RemoveButton>
+            <QuantityInput
+              size="small"
+              onIncrease={handleIncrease}
+              onDecrease={handleDecrease}
+              quantity={coffee.quantity}
+            />
+            <RemoveButton onClick={handleRemove}>
               <Trash size={16} />
               REMOVER
             </RemoveButton>
           </ActionsContainer>
         </div>
       </div>
-      <p>R$ 9,90</p>
+      <p>R$ {formattedPrice}</p>
     </CoffeeCartCardContainer>
   );
 }
