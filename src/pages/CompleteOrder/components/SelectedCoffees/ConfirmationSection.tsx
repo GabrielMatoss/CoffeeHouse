bfdf9631919ee8eea260a8c9c02504f09faae901
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-04-11 14:23:14 -0300

    feat(confirmationsection): add code to calculate total value of items and code indentation

--- src/pages/CompleteOrder/components/SelectedCoffees/ConfirmationSection.tsx
+++ src/pages/CompleteOrder/components/SelectedCoffees/ConfirmationSection.tsx
@@ -1,24 +1,39 @@
 import { Button } from "../../../../components/Button";
+import { useCart } from "../../../../hooks/useCart";
+import { formatMoney } from "../../../../utils/formatMoney";
 import { ConfirmationSectionContainer } from "./styles";
 
+const DELIVERY_PRICE = 3.5;
+
 export function ConfirmationSection() {
+  const { cartItemsTotal, cartQuantity } = useCart();
+  const cartTotal = DELIVERY_PRICE + cartItemsTotal;
+
+  const formattedItemsTotal = formatMoney(cartItemsTotal);
+  const formattedCartTotal = formatMoney(cartTotal);
+  const formatDeliveryPrice = formatMoney(DELIVERY_PRICE);
+
   return (
     <ConfirmationSectionContainer>
       <div>
         <p>Total de itens</p>
-        <span>R$ 9,90</span>
+        <span>R$ {formattedItemsTotal}</span>
       </div>
       <div>
         <p>Entrega</p>
-        <span>R$ 3,50</span>
+        <span>R$ {formatDeliveryPrice}</span>
       </div>
 
       <div>
         <h4>Total</h4>
-        <span className="totalValue">R$ 33,20</span>
+        <span className="totalValue">R$ {formattedCartTotal}</span>
       </div>
 
-      <Button text="Confirmar Pedido" />
+      <Button
+        text="Confirmar Pedido"
+        disabled={cartQuantity <= 0}
+        type="submit"
+      />
     </ConfirmationSectionContainer>
   );
 }
