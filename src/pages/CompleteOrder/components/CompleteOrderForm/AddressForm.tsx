4b0f9f19a027e238e6ccbd54ca337dcd509d1bb1
Gabriel Matos Lourenço <matos.gabriel2013@gmail.com>
2023-04-11 14:22:12 -0300

    feat(addressform): add error typing, and setting an error message with zod

--- src/pages/CompleteOrder/components/CompleteOrderForm/AddressForm.tsx
+++ src/pages/CompleteOrder/components/CompleteOrderForm/AddressForm.tsx
@@ -1,16 +1,35 @@
 import { Input } from "../../../../components/Input";
 import { AddressFormContainer } from "./styles";
+import { useFormContext } from "react-hook-form";
 
-export function AddressForm(){
-    return(
-        <AddressFormContainer>
-           <Input placeholder="CEP" className="cep" type="number"/>
-           <Input placeholder="Rua" className="rua" />
-           <Input placeholder="Número" type="number" />
-           <Input placeholder="Completemento" className="complemento" />
-           <Input placeholder="Bairro" />
-           <Input placeholder="Cidade" />
-           <Input placeholder="UF" />
-        </AddressFormContainer>
-    );
+interface ErrorsType {
+  errors: {
+    [key: string]: {
+      message: string;
+    };
+  };
+}
+
+export function AddressForm() {
+  const { register, formState } = useFormContext();
+
+  const { errors } = formState as unknown as ErrorsType;
+
+  return (
+    <AddressFormContainer>
+      <Input
+        placeholder="CEP"
+        className="cep"
+        type="number"
+        {...register("cep")}
+        error={errors.cep?.message}
+      />
+      <Input placeholder="Rua" className="rua" />
+      <Input placeholder="Número" type="number" />
+      <Input placeholder="Completemento" className="complemento" />
+      <Input placeholder="Bairro" />
+      <Input placeholder="Cidade" />
+      <Input placeholder="UF" />
+    </AddressFormContainer>
+  );
 }
